19ed86d1de47521d2763dbe274667db0086fe166
gaurav kumar <gauravkumar552@gmail.com>
2015-06-12 23:22:26 +0530

    Adds basic host file and requirement file

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/urlnote', methods=['POST'])
+def save_url():
+    # save link in db
+    return 'url saved'
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0')
